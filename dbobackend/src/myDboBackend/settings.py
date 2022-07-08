4505681fb0ea414d20e4df1354ef7fa17d1000c4
BLADINE <59175711+BLADINE@users.noreply.github.com>
2022-07-08 13:26:11 +0200

    test git command

--- dbobackend/src/myDboBackend/settings.py
+++ dbobackend/src/myDboBackend/settings.py
@@ -9,12 +9,13 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+#print("le chemin est ", BASE_DIR)
+#print("notre chemin est", BASE_DIR.parent.joinpath('mydatabase').resolve())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -25,7 +26,8 @@ SECRET_KEY = 'django-insecure-6g2ub9l$6e(vxtc$d_58snf^x2c1vn&zv_+rzi$_7$sm#ha8%q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost',
+                 '127.0.0.1']
 
 
 # Application definition
@@ -85,7 +87,8 @@ WSGI_APPLICATION = 'myDboBackend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR.parent.joinpath('mydatabase').resolve() / 'db.sqlite3',
     }
 }
 
